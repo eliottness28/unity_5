d0d98369128a10cf33d9f56ab452159205b5bb24
eliottness28 <eliottmax@hotmail.fr>
2015-06-01 19:23:43 +0200

    premier essai

---
+++ testCube/Assets/Scripts/ContrrolShooter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContrrolShooter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("test shooter");
+	}
+
+    void Déplacement()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            transform.position += new Vector3(0, -1f, 0);
+
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.position += new Vector3(0, 0, 1f);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            transform.position += new Vector3(0, 0, -1f);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            transform.position += new Vector3(-1f, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.position += new Vector3(1f, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            transform.position += new Vector3(0, 1f, 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Déplacement();
+	}
+}
---
+++ testCube/Assets/bougeouioui.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class bougeouioui : MonoBehaviour
+{
+
+
+    const float VITESSE_JOUEUR = 0.1f;
+    void Start()
+    {
+        print("test shooter");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("coll");
+    }
+
+    void Déplacement()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(0, 0, VITESSE_JOUEUR);
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.position += new Vector3(0, 0, -VITESSE_JOUEUR);
+
+        if (Input.GetKey(KeyCode.Z))
+            transform.position += new Vector3(-VITESSE_JOUEUR, 0, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position += new Vector3(VITESSE_JOUEUR, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Déplacement();
+
+    }
+     
+    //TODO insérer le décor du village viking
+    //but du jeu attraper des villagois
+
+}
